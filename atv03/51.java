7da02faf9948bf69191703617df135e6908fc30e
Carolina Silva <65463404+Carolina-Silva@users.noreply.github.com>
2022-08-29 23:00:43 -0300

    fim media

--- atv03/51.java
+++ atv03/51.java
@@ -4,6 +4,25 @@
 // prova de cinco alunos. Armazenar as notas da primeira e da segunda prova em
 // vetores distintos. Calcular e escrever a média de cada aluno.
 
-public class 51 {
-  
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int vetA[] = new int[5];
+        int vetB[] = new int[5];
+        System.out.println("Informe as notas dos alunos na primeira prova:");
+        for (int i = 0; i < 5; i++){
+            vetA[i] = sc.nextInt();
+        }
+        System.out.println("Agora da segunda prova:");
+        for (int i = 0; i < 5; i++){
+            vetB[i] = sc.nextInt();
+        }
+        
+        for(int i = 0; i < 5; i++){
+            int m = (vetB[i] + vetA[i]) /2;
+            System.out.printf("A média do aluno %d é : %d\n", (i+1), m );
+        }
+    }
 }
