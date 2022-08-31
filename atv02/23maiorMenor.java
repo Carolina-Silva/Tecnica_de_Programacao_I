6bfa4d9895ebccba7704c9ab01d4725174451ece
Carolina Silva <65463404+Carolina-Silva@users.noreply.github.com>
2022-08-30 22:21:24 -0300

    fimmmm

--- atv02/23maiorMenor.java
+++ atv02/23maiorMenor.java
@@ -1,27 +1,36 @@
 //Nome: Carolina Nascimento Silva
 //Ra: 2171392211001
-//Objetivo: Faça um programa na Linguagem Java que efetue a leitura de cinco números inteiros e identificar o maior e o menor valor. Não execute a ordenação de valores.
-import java.util.Scanner;
-import java.util.Arrays;
+//Objetivo:Faça um programa na Linguagem  Java  que efetue a leitura de cinco números inteiros e identificar o maior e o menor valor. Não execute a ordenação de valores.
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int vet [] = new int [5];
-        int i = 0, maior, menor;
         System.out.println("Entre com os valores:");
+        int i = 0;
         while (i< vet.length){
             vet[i++] = sc.nextInt();
-
-            maior = vet[i];
-            menor = vet[i];
-
-            
-
-
         }
-        System.out.printf("Maior número: %d",vet[2]);   
+        
+        int menor = vet[0];
+        for (i = 0; i < vet.length; i++){
+            if(menor >= vet[i]){
+                menor = vet[i];
+            }
+        }
+        
+        int maior = vet[0];
+        for (i = 0; i < vet.length; i++){
+            if(maior <= vet[i]){
+                maior = vet[i];
+            }
+        }
+        
+        System.out.print("Maior = ");
+        System.out.print(maior);
+        System.out.print("\nMenor = ");
+        System.out.print(menor);
+        
     }
 }
-
-//ajudaaaaa sem ordenação
